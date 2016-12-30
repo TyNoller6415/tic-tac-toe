d6d6e0f227a039d0ac8d1998ebfdc48742c890c0
TyNoller6415 <tynoller6415@gmail.com>
2016-12-29 21:57:20 -0700

    Added a scanner to my program.

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,8 +1,11 @@
 package ticTacToe;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Hello world");
 
 	}
